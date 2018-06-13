42598d9ce8bbdf1e8e0f676ab4b99a607ccab0f1
Javi Roman <jroman.espinar@gmail.com>
2018-06-13 14:58:41 +0200

    sample code for writing znodes in Zookeeper

---
+++ Java/Zookeeper/znodecopy/src/main/java/ZKCreate.java
@@ -0,0 +1,38 @@
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZKCreate {
+    // create static instance for zookeeper class.
+    private static ZooKeeper zk;
+
+    // create static instance for ZooKeeperConnection class.
+    private static ZooKeeperConnection conn;
+
+    // Method to create znode in zookeeper ensemble
+    public static void create(String path, byte[] data) throws
+            KeeperException,InterruptedException {
+        zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+    }
+
+    public static void main(String[] args) {
+
+        // znode path
+        String path = "/MyFirstZnode"; // Assign path to znode
+
+        String mydata = "my mierda";
+        // data in byte array
+        byte[] data = mydata.getBytes();
+
+        try {
+            conn = new ZooKeeperConnection();
+            zk = conn.connect("localhost");
+            create(path, data); // Create the data to the specified path
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage()); //Catch error message
+        }
+    }
+}
---
+++ Java/Zookeeper/znodecopy/src/main/java/ZooKeeperConnection.java
@@ -0,0 +1,40 @@
+// import java classes
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+// import zookeeper classes
+
+public class ZooKeeperConnection {
+
+    // declare zookeeper instance to access ZooKeeper ensemble
+    private ZooKeeper zoo;
+    final CountDownLatch connectedSignal = new CountDownLatch(1);
+
+    // Method to connect zookeeper ensemble.
+    public ZooKeeper connect(String host) throws IOException,InterruptedException {
+
+        zoo = new ZooKeeper(host,5000,new Watcher() {
+
+            public void process(WatchedEvent we) {
+
+                if (we.getState() == KeeperState.SyncConnected) {
+                    connectedSignal.countDown();
+                }
+            }
+        });
+
+        connectedSignal.await();
+        return zoo;
+    }
+
+    // Method to disconnect from zookeeper server
+    public void close() throws InterruptedException {
+        zoo.close();
+    }
+}
