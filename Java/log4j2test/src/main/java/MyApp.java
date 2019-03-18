a4df86b93b8a3e64b013c4bc77c8d537cb1c3a8c
Javi Roman <jroman.espinar@gmail.com>
2019-03-18 18:36:18 +0100

    more about log4j2

--- Java/log4j2test/src/main/java/MyApp.java
+++ Java/log4j2test/src/main/java/MyApp.java
@@ -3,6 +3,9 @@ import com.foo.Bar;
 // Import log4j classes.
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 public class MyApp {
     static Logger rootLogger = LogManager.getRootLogger();
@@ -13,15 +16,18 @@ public class MyApp {
     private static final Logger logger = LogManager.getLogger(MyApp.class);
 
     public static void main(String args[]) {
-        rootLogger.trace("root logger trace");
-        // Set up a simple configuration that logs on the console.
+        rootLogger.trace("root logger level -> {}.", rootLogger.getLevel());
+
+        logger.info("Logger name -> {}.", logger.getName());
+        logger.trace("Logger level -> {}.", logger.getLevel());
         logger.trace("Entering application (trace).");
+
         Bar bar = new Bar();
         if (!bar.doIt()) {
             logger.error("Didn't do it (error).");
         }
-        logger.trace("Exiting application (trace).");
 
+        logger.trace("Exiting application (trace).");
         logger.debug("This Will Be Printed On Debug");
         logger.info("This Will Be Printed On Info");
         logger.warn("This Will Be Printed On Warn");
--- Java/log4j2test/src/main/java/com/foo/Bar.java
+++ Java/log4j2test/src/main/java/com/foo/Bar.java
@@ -7,6 +7,8 @@ public class Bar {
     static final Logger logger = LogManager.getLogger(Bar.class.getName());
 
     public boolean doIt() {
+        logger.info("Logger name -> {}.", logger.getName());
+        logger.trace("Logger level -> {}.", logger.getLevel());
         logger.traceEntry();
         logger.error("Did it again! (error)");
         return logger.traceExit(false);
