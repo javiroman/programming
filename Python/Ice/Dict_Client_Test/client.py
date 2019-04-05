216fa58abf9aa9534a8e5c7688fcc49597000a1b
Javi Roman <jroman.espinar@gmail.com>
2019-04-05 08:33:01 +0200

    working example of async Ice python client and java server

--- Python/Ice/Dict_Client_Test/client.py
+++ Python/Ice/Dict_Client_Test/client.py
@@ -6,27 +6,30 @@ import Ice
 Ice.loadSlice('Hello.ice')
 import Demo
 
-def run(communicator):
-    hello = Demo.HelloPrx.checkedCast(communicator.propertyToProxy('Hello.Proxy'))
-    if not hello:
-        print(sys.args[0] + ": invalid proxy")
-        sys.exit(1)
+class MiddlewareClient:
 
-    try:
-            hello.sayHello(0)
-    except Ice.Exception as ex:
-        print(ex)
+    def __init__(self, args):
+        self.args = args
+
+    def run(self, communicator):
+        hello = Demo.HelloPrx.checkedCast(
+            communicator.propertyToProxy('Hello.Proxy'))
+        if not hello:
+            print(sys.args[0] + ": invalid proxy")
+            sys.exit(1)
+
+        if len(self.args) > 1 and self.args[1] == "stop":
+            hello.shutdown()
+            sys.exit(1)
+        try:
+                hello.sayHello(0)
+        except Ice.Exception as ex:
+            print(ex)
 
 #
 # Ice.initialize returns an initialized Ice communicator,
 # the communicator is destroyed once it goes out of scope.
 #
 with Ice.initialize(sys.argv, "config.client") as communicator:
-
-    # The communicator initialization removes all Ice-related arguments from argv
-    #
-    if len(sys.argv) > 1:
-        print(sys.argv[0] + ": too many arguments")
-        sys.exit(1)
-
-    run(communicator)
+    mdc = MiddlewareClient(sys.argv)
+    mdc.run(communicator)
