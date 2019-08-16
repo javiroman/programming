34241e125734f374ca32691de4bd26c945e01cba
Javi Roman <jroman.espinar@gmail.com>
2019-08-16 11:15:39 +0200

    go ahead with the example reactjs

--- Reactjs/sportsstore/server.js
+++ Reactjs/sportsstore/server.js
@@ -1,6 +1,15 @@
+// Express is a minimal and flexible Node.js web application framework
 const express = require("express");
+
+// Simulate a backend REST service to deliver some data in JSON format to the front-end
+// https://medium.com/codingthesmartway-com-blog/create-a-rest-api-with-json-server-36da8680136d
 const jsonServer = require("json-server");
+
+// File watcher: https://github.com/paulmillr/chokidar
 const chokidar = require("chokidar");
+
+// Enable: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+// https://github.com/expressjs/cors
 const cors = require("cors");
 
 const fileName = process.argv[2] || "./data.js";
@@ -30,3 +39,5 @@ chokidar.watch(fileName).on("change", () => {
     createServer();
     console.log("Reloading  web service data complete.");
 });
+
+app.listen(port, () => console.log('Web service running on port ${port}'));
---
+++ Reactjs/sportsstore/src/data/Types.js
@@ -0,0 +1,10 @@
+// data types
+export const DataTypes = {
+    PRODUCTS: "products",
+    CATEGORIES: "categories"
+}
+
+// an action
+export const ActionTypes = {
+    DATA_LOAD: "data_load"
+}
