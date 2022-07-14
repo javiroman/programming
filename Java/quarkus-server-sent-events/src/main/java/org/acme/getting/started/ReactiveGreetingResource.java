b744c42b4d18797d273c747d678088446298827e
Javi Roman <jroman.espinar@gmail.com>
2022-07-14 10:15:30 +0200

    Quarkus Server Sent Events

---
+++ Java/quarkus-server-sent-events/src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
@@ -0,0 +1,47 @@
+package org.acme.getting.started;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+
+@Path("/hello")
+public class ReactiveGreetingResource {
+
+    @Inject
+    ReactiveGreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public Uni<String> greeting(String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{count}/{name}")
+    public Multi<String> greetings(int count, String name) {
+        return service.greetings(count, name);
+    }
+
+    @POST
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestStreamElementType(MediaType.APPLICATION_JSON)
+    @Path("/stream/{count}/{name}")
+    public Multi<String> greetingsAsStream(int count, String name) {
+        return service.greetings(count, name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
